9d9c9d9e00937d78b9db4885dd7b376f1f729a9b
Mahesh SK <maheshsk019@gmail.com>
2019-08-06 08:01:53 +0530

    Added Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component'; // Import home component
+import { ListComponent } from './list/list.component'; // Import List Component
+
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },              // add default path
+  { path: 'list', component: ListComponent }           // add list path
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
